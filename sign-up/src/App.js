816cef47692c4f59ee3252f968c66c2155b5d12a
Nima sherpa <Toplouie78@gmail.com>
2021-12-18 12:52:33 -0500

    fixing the layout

--- sign-up/src/App.js
+++ sign-up/src/App.js
@@ -7,16 +7,29 @@ function App() {
 				<div className="insideContainer">
 					<h2>Join the </h2>
 					<h2>community</h2>
-					<p>take your art to the next level. Get it</p>
+					<p>Take your art to the next level. Get it</p>
 					<p>seen by millions of people</p>
 					<button>join with Facebook</button>
 					<p>or</p>
 					<form>
 						<div>
-							<input type="text" placeHolder="First name" />
-							<input type="text" placeHolder="Last name" />
+							<input
+								className="firstInput"
+								type="text"
+								placeHolder="First name"
+							/>
+							<input
+								className="firstInput"
+								type="text"
+								placeHolder="Last name"
+							/>
 						</div>
+						<br />
+
 						<input type="text" placeHolder="Email" />
+						<br />
+						<br />
+
 						<input type="text" placeHolder="password" />
 					</form>
 				</div>
