a78e3cf3672454d62a637c7f237affadb07897c0
Nima sherpa <Toplouie78@gmail.com>
2021-12-18 12:41:49 -0500

    layout is done

--- sign-up/src/App.js
+++ sign-up/src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<div className="outsideContainer">
+				<div className="insideContainer">
+					<h2>Join the </h2>
+					<h2>community</h2>
+					<p>take your art to the next level. Get it</p>
+					<p>seen by millions of people</p>
+					<button>join with Facebook</button>
+					<p>or</p>
+					<form>
+						<div>
+							<input type="text" placeHolder="First name" />
+							<input type="text" placeHolder="Last name" />
+						</div>
+						<input type="text" placeHolder="Email" />
+						<input type="text" placeHolder="password" />
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
